9e32e488d58f1f6bf1c6b64ba4f92efe9e62c61e
al-ʿAnqāʾ <ahmed.m.almousawi@gmail.com>
2022-06-02 15:21:20 -0600

    began django integration

---
+++ SAMARADeployment/software/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class WikiPage(models.Model):
+    
+    url = models.CharField(max_length=300)
+    pagetype = models.CharField(max_length=10)
+    teamname = models.CharField(max_length=30)
+    year = models.CharField(max_length=4)
+    pagetext = models.TextField()
+
+
+
+# Create your models here.
