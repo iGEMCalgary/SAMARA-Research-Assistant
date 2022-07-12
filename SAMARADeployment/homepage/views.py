9a8ce83d2b1ba5d296a11ce702e67ddf1da6f198
al-ʿAnqāʾ <ahmed.m.almousawi@gmail.com>
2022-07-12 08:57:20 -0600

    worked on initial homepage

--- SAMARADeployment/homepage/views.py
+++ SAMARADeployment/homepage/views.py
@@ -1,7 +1,4 @@
-import re
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import RequestContext, loader
 
 # Create your views here.
 def homepage(request):
